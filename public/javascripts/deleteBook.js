cdce463ccd164fb345ad1aedb8919703c39db726
Med <mehdi.gacem@versusmind.eu>
2019-03-08 12:23:44 +0100

    create an ajax update request

---
+++ public/javascripts/deleteBook.js
@@ -0,0 +1,16 @@
+function sendDeleteRequest(deleteUrl, redirectUrl){
+//    $(document).ready(function(){
+        console.log("Delete Book "+deleteUrl);
+         $.ajax({
+                url: "/books/delete/100",//
+                method: "DELETE",
+                success: function(){
+                    window.location = redirectUrl;
+                },
+                error: function(jqXHR){
+                    alert("XHR Error : "+jqXHR.status+" => "+ jqXHR.statusText);
+                    window.location.reload();
+                }
+        });
+//    });
+}
